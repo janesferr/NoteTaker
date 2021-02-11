8a7777e7a59482de84f96264047769ef97aa080b
jansferr <sferrazzajane@gmail.com>
2021-02-11 17:02:07 -0500

    got everything working except the delete

--- server.js
+++ server.js
@@ -12,41 +12,31 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("public"));
 // Star Wars Characters (DATA)
 
-// const characters = [
-//   {
-//     routeName: 'yoda',
-//     name: 'Yoda',
-//     role: 'Jedi Master',
-//     age: 900,
-//     forcePoints: 2000,
-//   },
-//   {
-//     routeName: 'darthmaul',
-//     name: 'Darth Maul',
-//     role: 'Sith Lord',
-//     age: 200,
-//     forcePoints: 1200,
-//   },
-//   {
-//     routeName: 'obiwankenobi',
-//     name: 'Obi Wan Kenobi',
-//     role: 'Jedi Master',
-//     age: 55,
-//     forcePoints: 1350,
-//   },
-// ];
+let notes = [
+    {
+        "title":"Test Title",
+        "text":"Test text"
+    }
+]
 
 // Routes
-
+app.use("/public", express.static('./public/'));
 // Basic route that sends the user first to the AJAX Page
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
-// app.get('/add', (req, res) => res.sendFile(path.join(__dirname, 'add.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+
+app.post('/public', (req, res) => res.sendFile(path.join(__dirname, './public/assets/js/index/js')));
+
 
 // // Displays all characters
 // app.get('/api/characters', (req, res) => res.json(characters));
+app.get('/api/notes', (req, res) => res.json(notes));
+
+
 
 // // Displays a single character, or returns false
 // app.get('/api/characters/:character', (req, res) => {
@@ -66,6 +56,23 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 //   return res.json(false);
 // });
+app.get('/api/notes/:id', (req, res) => {
+  const chosen = req.params.character;
+
+  console.log(chosen);
+
+  /* Check each character routeName and see if the same as "chosen"
+   If the statement is true, send the character back as JSON,
+   otherwise tell the user no character was found */
+
+  for (let i = 0; i < notes.length; i++) {
+    if (chosen === notes[i].routeName) {
+      return res.json(notes[i]);
+    }
+  }
+
+  return res.json(false);
+});
 
 // // Create New Characters - takes in JSON input
 // app.post('/api/characters', (req, res) => {
@@ -81,6 +88,30 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 //   characters.push(newCharacter);
 //   res.json(newCharacter);
 // });
+// app.post('/api/notes', (req, res) => res.json(notes));
+app.post('/api/notes', (req, res) => {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  let note = req.body;
+
+  console.log(note);
+
+  // We then add the json the user sent to the character array
+  notes.push(note);
+
+  // We then display the JSON to the users
+  res.json(note);
+});
+
+app.post('/api/notes/:id', (req, res) => {
+    let id = req.params.id;
+    delete note[id];
+   console.log(notes);
+
+    
+    res.json({ ok: true});
+ });
+
 
 // Starts the server to begin listening
 
