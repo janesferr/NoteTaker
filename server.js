72dd161d8a493731ad7e43b57db3e69b38530c28
jansferr <sferrazzajane@gmail.com>
2021-02-11 23:35:20 -0500

    notworking

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const path = require('path');
+let notes = require("./db/db.json");
+const fs = require('fs');
 
 // Sets up the Express App
 
@@ -15,12 +17,14 @@ app.use(express.json());
 app.use(express.static("public"));
 // Star Wars Characters (DATA)
 
-let notes = [
-    {
-        "title":"Test Title",
-        "text":"Test text"
-    }
-]
+// let notes = [
+//     {
+//         "title":"Test Title",
+//         "text":"Test text"
+//     }
+// ]
+fs.writeFileSync(path.resolve(__dirname, './db/db.json'), JSON.stringify(notes));
+
 
 // Routes
 app.use("/public", express.static('./public/'));
@@ -38,24 +42,7 @@ app.get('/api/notes', (req, res) => res.json(notes));
 
 
 
-// // Displays a single character, or returns false
-// app.get('/api/characters/:character', (req, res) => {
-//   const chosen = req.params.character;
 
-//   console.log(chosen);
-
-//   /* Check each character routeName and see if the same as "chosen"
-//    If the statement is true, send the character back as JSON,
-//    otherwise tell the user no character was found */
-
-//   for (let i = 0; i < characters.length; i++) {
-//     if (chosen === characters[i].routeName) {
-//       return res.json(characters[i]);
-//     }
-//   }
-
-//   return res.json(false);
-// });
 app.get('/api/notes/:id', (req, res) => {
   const chosen = req.params.character;
 
@@ -72,24 +59,17 @@ app.get('/api/notes/:id', (req, res) => {
   }
 
   return res.json(false);
+  // const found = notes.some(notes => notes.id === parseInt(req.params.id));
+  // if(found){
+  //   res.json(notes.filter(notes => notes.id ===parseInt(req.params.id)));
+
+  // }else{
+  //   res.status(400).json({msg: `No member with teh id of ${req.params.id}`});
+  // }
 });
+// app.post('/api/notes/', (req, res) => res.json(notes));
 
-// // Create New Characters - takes in JSON input
-// app.post('/api/characters', (req, res) => {
-//   // req.body hosts is equal to the JSON post sent from the user
-//   // This works because of our body parsing middleware
-//   const newCharacter = req.body;
-
-//   // Using a RegEx Pattern to remove spaces from newCharacter
-//   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-//   newCharacter.routeName = newCharacter.name.replace(/\s+/g, '').toLowerCase();
-//   console.log(newCharacter);
-
-//   characters.push(newCharacter);
-//   res.json(newCharacter);
-// });
-// app.post('/api/notes', (req, res) => res.json(notes));
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes/:id', (req, res) => {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
   let note = req.body;
@@ -103,14 +83,22 @@ app.post('/api/notes', (req, res) => {
   res.json(note);
 });
 
-app.post('/api/notes/:id', (req, res) => {
-    let id = req.params.id;
-    delete note[id];
-   console.log(notes);
+app.delete('/api/notes/:id', (req, res) => {
+  //   let id = req.params.id;
+  //   if(delete notes.id){
+  //       res.json(notes.id);
+  //   }
+  //  console.log(notes);
 
     
-    res.json({ ok: true});
- });
+    // res.json({ ok: true});
+    // res.json(notes);
+// for(let i = 0; i <notes.length; i++){
+//     if(notes[i].id === key){
+//         notes.splice(i,1);
+//     }
+// }
+});
 
 
 // Starts the server to begin listening
